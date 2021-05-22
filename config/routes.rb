ca4b9cbbdb40d7890d94c850ab593e3c42088830
shouzou.nozaki <shouzou.nozaki@techacademy.jp>
2021-05-22 08:03:05 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
+  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
+  get 'signup', to: 'users#new'
+  resources :users, only:[:create,:show,:index] 
   
 end
